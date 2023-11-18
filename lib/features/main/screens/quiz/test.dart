548f8191fa29826012e2bbe24b5e86b8a26ffd62
Truong-Quang-Dat <100523403+Truong-Quang-Dat@users.noreply.github.com>
2023-11-18 15:59:06 +0700

    finish testExam

--- lib/features/main/screens/quiz/test.dart
+++ lib/features/main/screens/quiz/test.dart
@@ -18,14 +18,18 @@ class _TestExamScreenState extends State<TestExamScreen> {
   // List<int> selectedAnswers = List.generate(questions.length, (index) => -1);
   late List<TestExamQuestion> questions;
   late List<int> selectedAnswers;
+  late List<bool> checkIfTheAnswerIsTrueOfFalse;
   late Timer timer;
   int totalTimeInSeconds = 30;
+  bool isSubmit = false;
+  int score = 0;
 
   @override
   void initState() {
     super.initState();
     questions = widget.questions;
     selectedAnswers = List.generate(questions.length, (index) => -1);
+    checkIfTheAnswerIsTrueOfFalse = List.generate(questions.length, (index) => false);
 
     timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
       if (totalTimeInSeconds > 0) {
@@ -40,36 +44,39 @@ class _TestExamScreenState extends State<TestExamScreen> {
   }
 
   void checkSubmit() {
-    if (selectedAnswers.contains(-1)) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Lỗi'),
-            content: const Text(
-                'Vui lòng trả lời hết các câu hỏi trước khi nộp bài.'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('Đóng'),
-              ),
-            ],
-          );
-        },
-      );
-    } else {
-      submitQuiz();
+    if(!isSubmit) {
+      if (selectedAnswers.contains(-1)) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Lỗi'),
+              content: const Text(
+                  'Vui lòng trả lời hết các câu hỏi trước khi nộp bài.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Đóng'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        submitQuiz();
+      }
     }
   }
 
   void submitQuiz() {
     timer.cancel();
-    int score = 0;
+    score = 0;
 
     for (int i = 0; i < questions.length; i++) {
       if (selectedAnswers[i] == questions[i].correctAnswerIndex) {
+        checkIfTheAnswerIsTrueOfFalse[i] = true;
         score++;
       }
     }
@@ -83,6 +90,9 @@ class _TestExamScreenState extends State<TestExamScreen> {
           actions: [
             TextButton(
               onPressed: () {
+                setState(() {
+                  isSubmit = true;
+                });
                 Navigator.of(context).pop();
               },
               child: const Text('Đóng'),
@@ -111,6 +121,35 @@ class _TestExamScreenState extends State<TestExamScreen> {
           ),
         ),
         backgroundColor: Colors.red,
+        actions: [
+          if(!isSubmit)
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: ElevatedButton(
+              onPressed: checkSubmit,
+              child: const Text(
+                '    Nộp bài   ',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          )
+          else
+            Padding(
+              padding: const EdgeInsets.only(right: 22),
+              child: Text(
+                'Điểm: $score/${questions.length}',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            )
+        ],
+
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -141,33 +180,60 @@ class _TestExamScreenState extends State<TestExamScreen> {
                       style: const TextStyle(
                           fontSize: 18, fontWeight: FontWeight.bold),
                     ),
-                    Column(
-                      children: List.generate(
-                        questions[index].options.length,
-                        (optionIndex) {
-                          return RadioListTile(
-                            title: Text(questions[index].options[optionIndex]),
-                            value: optionIndex,
-                            groupValue: selectedAnswers[index],
-                            onChanged: (value) {
-                              setState(() {
-                                selectedAnswers[index] = value!;
-                              });
-                            },
-                          );
-                        },
-                      ),
-                    ),
+                    if(!isSubmit)
+                      Column(
+                        children: List.generate(
+                          questions[index].options.length,
+                          (optionIndex) {
+                            return RadioListTile(
+                              title: Text(questions[index].options[optionIndex]),
+                              value: optionIndex,
+                              groupValue: selectedAnswers[index],
+                              onChanged: (value) {
+                                setState(() {
+                                  selectedAnswers[index] = value!;
+                                });
+                              },
+                            );
+                          },
+                        ),
+                      )
+                    else
+                      Column(
+                        children: List.generate(
+                          questions[index].options.length,
+                              (optionIndex) {
+                            return RadioListTile(
+                              title: Text(
+                                questions[index].options[optionIndex],
+                                style: TextStyle(
+                                  color: (optionIndex == selectedAnswers[index] && checkIfTheAnswerIsTrueOfFalse[index])
+                                      ? Colors.green
+                                      : (optionIndex == selectedAnswers[index] && !checkIfTheAnswerIsTrueOfFalse[index])
+                                      ? Colors.red
+                                      : (optionIndex == questions[index].correctAnswerIndex)
+                                      ? Colors.green
+                                      : Colors.grey,
+                                )
+                              ),
+                              value: optionIndex,
+                              groupValue: selectedAnswers[index],
+                              onChanged: null,
+                            );
+                          },
+                        ),
+                      )
                   ],
                 );
               },
             ),
+            if(isSubmit)
             Container(
               alignment: Alignment.bottomRight,
               child: ElevatedButton(
-                onPressed: checkSubmit,
+                onPressed: () {},
                 child: const Text(
-                  'Nộp Bài',
+                  ' Xem bình luận ',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 16,
