a3f33a763f459b6bf6b4b67566d08032434581f5
unknown <stkey002@gmail.com>
2023-11-20 23:52:06 +0700

    nothing

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:ui_project_hochiminh_museum/features/admin/screens/create_news/create_news.dart';
+import 'package:ui_project_hochiminh_museum/features/admin/screens/create_news/text_editor_screen.dart';
 import 'package:ui_project_hochiminh_museum/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:ui_project_hochiminh_museum/utils/constants/text_strings.dart';
 import 'package:ui_project_hochiminh_museum/utils/theme/theme.dart';
@@ -16,7 +18,7 @@ class App extends StatelessWidget {
       darkTheme: TAppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
       // initialBinding: GeneralBindings(),
-      home: const OnBoardingScreen(),
+      home: const TextEditorScreen(),
       // home: const MuseumThreeDimensionScreen(),
     );
   }
