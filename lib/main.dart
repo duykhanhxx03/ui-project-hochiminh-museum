f161b51e17788323cc5f89bd1f2eacaa3f3b6f6e
Trần Duy Khánh <99308937+duykhanhxx03@users.noreply.github.com>
2023-11-26 09:30:33 +0700

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get_core/get_core.dart';
 import 'package:get/get_instance/get_instance.dart';
 import 'package:ui_project_hochiminh_museum/app.dart';
 import 'package:ui_project_hochiminh_museum/firebase_options.dart';
 import 'package:ui_project_hochiminh_museum/repository/authentication_repository/authentication_repository.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then(
     (value) => Get.put(
       AuthenticationRepository(),
     ),
