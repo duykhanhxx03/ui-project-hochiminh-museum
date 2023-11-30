1aa6eadf63bcdca5c798251ab2559b7fc6325ec6
Truong-Quang-Dat <100523403+Truong-Quang-Dat@users.noreply.github.com>
2023-11-30 23:10:55 +0700

    Search Screen find by title

--- lib/features/main/screens/home/search.dart
+++ lib/features/main/screens/home/search.dart
@@ -1,30 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ui_project_hochiminh_museum/repository/news_repository/news_repository.dart';
 
-void main() {
-  runApp(const SearchScreen());
-}
+import '../news/models/news_model.dart';
+import '../news/news_description.dart';
+import '../news/widgets/news_home/innotice_news.dart';
 
 class SearchScreen extends StatefulWidget {
-  const SearchScreen({super.key});
+  const SearchScreen({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<SearchScreen> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<SearchScreen> {
+  late List<NewsModel> newsListData = [];
+  final controller = Get.put(NewsRepository());
+
+  void searchFromFirebase(String searchTerm) async {
+    List<NewsModel> result =
+        await controller.searchNews(searchTerm);
+
+    setState(() {
+      newsListData = result;
+      print("Độ dài: ${newsListData.length}");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Search'),
+        title: const Text('Search Post'),
       ),
-      body: TextField(
-        decoration: const InputDecoration(
-          hintText: 'Tìm kiếm...',
-        ),
-        onChanged: (value) {
-          print(value);
-        },
+      body: Column(
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Tìm kiếm...',
+            ),
+            onChanged: (value) {
+              searchFromFirebase(value);
+            },
+          ),
+
+          Align(
+            alignment: Alignment.centerLeft, // Align text to the left
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Số bài viết trùng khớp: ${newsListData.length}',
+                style: const TextStyle(fontWeight: FontWeight.bold, fontStyle: FontStyle.italic)),
+            ),
+          ),
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: newsListData.length,
+              shrinkWrap: true,
+              physics: const AlwaysScrollableScrollPhysics(),
+              itemBuilder: (_, index) => InnoticeNews(
+                date: newsListData[index].date,
+                isNetworkImage: true,
+                onPressed: () => Get.to(NewsDescriptionScreen(
+                    newsContent: newsListData[index].newsContent)),
+                thumbnailUrl: newsListData[index].thumbnailUrl,
+                title: newsListData[index].title,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
